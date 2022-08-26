126f750d5c5c962eddc1e54d12a6a22009fc1d05
NielsRogge <niels.rogge1@gmail.com>
2022-08-26 12:32:00 +0000

    Apply suggestions from code review

--- utils/check_repo.py
+++ utils/check_repo.py
@@ -185,6 +185,7 @@ IGNORE_NON_AUTO_CONFIGURED = PRIVATE_MODELS.copy() + [
     "DPTForDepthEstimation",
     "DecisionTransformerGPT2Model",
     "GLPNForDepthEstimation",
+    "ViltForQuestionAnswering",
     "ViltForImagesAndTextClassification",
     "ViltForImageAndTextRetrieval",
     "ViltForTokenClassification",
