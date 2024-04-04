265ac52b58784496c0632fd14f206698078f3b39
Niels <niels.rogge1@gmail.com>
2024-04-04 17:38:29 +0200

    Add apply_chat_template

--- src/transformers/models/llava/test.py
+++ src/transformers/models/llava/test.py
@@ -11,7 +11,7 @@ processor = LlavaProcessor.from_pretrained("llava-hf/llava-1.5-7b-hf")
 image1 = Image.open(requests.get("https://llava-vl.github.io/static/images/view.jpg", stream=True).raw)
 image2 = Image.open(requests.get("http://images.cocodataset.org/val2017/000000039769.jpg", stream=True).raw)
 
-inputs = [
+messages = [
     [
         {"input": "image", "content": image1},
         {
@@ -25,7 +25,7 @@ inputs = [
     ],
 ]
 
-features = processor(inputs, padding=True, return_tensors="pt")
+features = processor.apply_chat_template(messages, padding=True, return_tensors="pt")
 
 
 # assert
@@ -33,19 +33,19 @@ from huggingface_hub import hf_hub_download
 
 
 filepath = hf_hub_download("nielsr/test-image", "llava_inputs.pt", repo_type="dataset")
-original_inputs = torch.load(filepath)
+original_messages = torch.load(filepath)
 
 for k, v in features.items():
     if k == "input_ids":
         for i in range(v.shape[0]):
             print(repr(processor.decode(v[i])))
-            print(repr(processor.decode(original_inputs[k][i])))
-    assert torch.allclose(v.float(), original_inputs[k].float())
+            print(repr(processor.decode(original_messages[k][i])))
+    assert torch.allclose(v.float(), original_messages[k].float())
 
 
 # test non-batched
 
-inputs = [
+messages = [
     [
         {"input": "image", "content": image1},
         {
@@ -65,7 +65,7 @@ inputs = [
     ],
 ]
 
-features = processor(inputs, padding=True, return_tensors="pt")
+features = processor.apply_chat_template(messages, padding=True, return_tensors="pt")
 
 # should create this:
 "USER: <image>\nThis is a cat\n<image>\nThis is a dog\n<image>\nWhat is this?\nASSISTANT:"
@@ -78,4 +78,4 @@ print(repr(processor.decode(features["input_ids"][0])))
 # if k == "input_ids":
 #     for i in range(v.shape[0]):
 #         print(repr(processor.decode(v[i])))
-#         print(repr(processor.decode(original_inputs[k][i])))
+#         print(repr(processor.decode(original_messages[k][i])))
