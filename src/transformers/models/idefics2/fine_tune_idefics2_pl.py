189722c4ef284c4b8924fda6e35bb122c7e933a1
Niels Rogge <niels.rogge1@gmail.com>
2024-05-04 21:01:17 +0200

    Add script

---
+++ src/transformers/models/idefics2/fine_tune_idefics2_pl.py
@@ -0,0 +1,354 @@
+import json
+import random
+from typing import Any, List, Dict
+import re
+from nltk import edit_distance
+
+from datasets import load_dataset
+from peft import LoraConfig
+from transformers import AutoProcessor, BitsAndBytesConfig, Idefics2ForConditionalGeneration
+
+import torch
+from torch.utils.data import Dataset, DataLoader
+
+import numpy as np
+
+import lightning as L
+from lightning.pytorch.callbacks import Callback
+from lightning.pytorch.loggers import WandbLogger
+from lightning.pytorch.callbacks.early_stopping import EarlyStopping
+
+wandb_logger = WandbLogger(project="Idefics2-PL", name="demo-run-cord")
+
+## load dataset
+
+dataset = load_dataset("naver-clova-ix/cord-v2")
+
+## load model
+
+lora_config = LoraConfig(
+    r=8,
+    lora_alpha=8,
+    lora_dropout=0.1,
+    target_modules='.*(text_model|modality_projection|perceiver_resampler).*(down_proj|gate_proj|up_proj|k_proj|q_proj|v_proj|o_proj).*$',
+    use_dora=False,
+    init_lora_weights="gaussian"
+)
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=torch.float16
+)
+
+model = Idefics2ForConditionalGeneration.from_pretrained(
+    "HuggingFaceM4/idefics2-8b",
+    torch_dtype=torch.float16,
+    quantization_config=bnb_config,
+)
+model.add_adapter(lora_config)
+model.enable_adapters()
+
+## Create a PyTorch Dataset
+
+processor = AutoProcessor.from_pretrained("HuggingFaceM4/idefics2-8b", do_image_splitting=False)
+
+added_tokens = []
+
+
+class Idefics2Dataset(Dataset):
+    """
+    PyTorch Dataset for Idefics2. This class takes a HuggingFace Dataset as input.
+    
+    Each row, consists of image path(png/jpg/jpeg) and gt data (json/jsonl/txt).
+    """
+
+    def __init__(
+        self,
+        dataset_name_or_path: str,
+        split: str = "train",
+        sort_json_key: bool = True,
+    ):
+        super().__init__()
+
+        self.split = split
+        self.sort_json_key = sort_json_key
+
+        self.dataset = load_dataset(dataset_name_or_path, split=self.split)
+        self.dataset_length = len(self.dataset)
+
+        self.gt_token_sequences = []
+        for sample in self.dataset:
+            ground_truth = json.loads(sample["ground_truth"])
+            if "gt_parses" in ground_truth:  # when multiple ground truths are available, e.g., docvqa
+                assert isinstance(ground_truth["gt_parses"], list)
+                gt_jsons = ground_truth["gt_parses"]
+            else:
+                assert "gt_parse" in ground_truth and isinstance(ground_truth["gt_parse"], dict)
+                gt_jsons = [ground_truth["gt_parse"]]
+
+            self.gt_token_sequences.append(
+                [
+                    self.json2token(
+                        gt_json,
+                        update_special_tokens_for_json_key=self.split == "train",
+                        sort_json_key=self.sort_json_key,
+                    )
+                    for gt_json in gt_jsons  # load json from list of json
+                ]
+            )
+
+    def json2token(self, obj: Any, update_special_tokens_for_json_key: bool = True, sort_json_key: bool = True):
+        """
+        Convert an ordered JSON object into a token sequence
+        """
+        if type(obj) == dict:
+            if len(obj) == 1 and "text_sequence" in obj:
+                return obj["text_sequence"]
+            else:
+                output = ""
+                if sort_json_key:
+                    keys = sorted(obj.keys(), reverse=True)
+                else:
+                    keys = obj.keys()
+                for k in keys:
+                    if update_special_tokens_for_json_key:
+                        self.add_tokens([fr"<s_{k}>", fr"</s_{k}>"])
+                    output += (
+                        fr"<s_{k}>"
+                        + self.json2token(obj[k], update_special_tokens_for_json_key, sort_json_key)
+                        + fr"</s_{k}>"
+                    )
+                return output
+        elif type(obj) == list:
+            return r"<sep/>".join(
+                [self.json2token(item, update_special_tokens_for_json_key, sort_json_key) for item in obj]
+            )
+        else:
+            obj = str(obj)
+            if f"<{obj}/>" in added_tokens:
+                obj = f"<{obj}/>"  # for categorical special tokens
+            return obj
+    
+    def add_tokens(self, list_of_tokens: List[str]):
+        """
+        Add special tokens to tokenizer and resize the token embeddings of the decoder
+        """
+        newly_added_num = processor.tokenizer.add_tokens(list_of_tokens)
+        if newly_added_num > 0:
+            model.resize_token_embeddings(len(processor.tokenizer))
+            added_tokens.extend(list_of_tokens)
+    
+    def __len__(self) -> int:
+        return self.dataset_length
+
+    def __getitem__(self, idx: int) -> Dict:
+        """
+        Load image from image_path of given dataset_path and convert into input_tensor and labels
+        Convert gt data into input_ids (tokenized string)
+        Returns:
+            input_tensor : preprocessed image
+            input_ids : tokenized gt_data
+            labels : masked labels (model doesn't need to predict prompt and pad token)
+        """
+        sample = self.dataset[idx]
+
+        # inputs
+        image = sample["image"]
+        target_sequence = random.choice(self.gt_token_sequences[idx])  # can be more than one, e.g., DocVQA Task 1
+        
+        return image, target_sequence
+    
+train_dataset = Idefics2Dataset("naver-clova-ix/cord-v2",  split="train", sort_json_key=False)
+val_dataset = Idefics2Dataset("naver-clova-ix/cord-v2", split="validation", sort_json_key=False)
+
+## Create a PyTorch DataLoader
+
+image_token_id = processor.tokenizer.additional_special_tokens_ids[processor.tokenizer.additional_special_tokens.index("<image>")]
+
+
+def train_collate_fn(examples):
+    texts = []
+    images = []
+    for example in examples:
+        image, ground_truth = example
+        messages = [
+            {
+                "role": "user",
+                "content": [
+                    {"type": "text", "text": "Extract JSON."},
+                    {"type": "image"},
+                ]
+            },
+            {
+                "role": "assistant",
+                "content": [
+                    {"type": "text", "text": ground_truth}
+                ]
+            }
+        ]
+        text = processor.apply_chat_template(messages, add_generation_prompt=False)
+        texts.append(text.strip())
+        images.append([image])
+
+    batch = processor(text=texts, images=images, return_tensors="pt", padding=True)
+
+    labels = batch["input_ids"].clone()
+    labels[labels == processor.tokenizer.pad_token_id] = image_token_id
+    batch["labels"] = labels
+
+    input_ids = batch["input_ids"]
+    attention_mask = batch["attention_mask"]
+    pixel_values = batch["pixel_values"]
+    pixel_attention_mask = batch["pixel_attention_mask"]
+    labels = batch["labels"]
+    
+    return input_ids, attention_mask, pixel_values, pixel_attention_mask, labels
+
+
+def eval_collate_fn(examples):
+    
+    # we feed the prompt to the model
+    images = []
+    texts = []
+    answers = []
+    for example in examples:
+        image, ground_truth = example
+        messages = [
+            {
+                "role": "user",
+                "content": [
+                    {"type": "text", "text": "Extract JSON."},
+                    {"type": "image"},
+                ]
+            },
+        ]
+        text = processor.apply_chat_template(messages, add_generation_prompt=True)
+        images.append([image])
+        texts.append(text.strip())
+        answers.append(ground_truth)
+
+    batch = processor(text=texts, images=images, return_tensors="pt", padding=True)
+
+    input_ids = batch["input_ids"]
+    attention_mask = batch["attention_mask"]
+    pixel_values = batch["pixel_values"]
+    pixel_attention_mask = batch["pixel_attention_mask"]
+    
+    return input_ids, attention_mask, pixel_values, pixel_attention_mask, answers
+
+## Create a PyTorch Lightning Module
+
+class Idefics2ModelPLModule(L.LightningModule):
+    def __init__(self, config, processor, model):
+        super().__init__()
+        self.config = config
+        self.processor = processor
+        self.model = model
+
+        self.batch_size = config.get("batch_size")
+
+    def training_step(self, batch, batch_idx):
+
+        input_ids, attention_mask, pixel_values, pixel_attention_mask, labels = batch
+
+        outputs = self.model(input_ids=input_ids,
+                                attention_mask=attention_mask,
+                                pixel_values=pixel_values,
+                                pixel_attention_mask=pixel_attention_mask,
+                                labels=labels)
+        loss = outputs.loss
+        
+        self.log("train_loss", loss)
+        
+        return loss
+
+    def validation_step(self, batch, batch_idx, dataset_idx=0):
+        
+        input_ids, attention_mask, pixel_values, pixel_attention_mask, answers = batch
+
+        # autoregressively generate token IDs
+        generated_ids = model.generate(input_ids=input_ids, attention_mask=attention_mask,
+                                       pixel_values=pixel_values, pixel_attention_mask=pixel_attention_mask,
+                                       max_new_tokens=768)
+        # turn them back into text, chopping of the prompt
+        # important: we don't skip special tokens here, because we want to see them in the output
+        generated_texts = processor.batch_decode(generated_ids[:, input_ids.size(1):])
+    
+        predictions = []
+        for seq in generated_texts:
+            seq = seq.replace(self.processor.tokenizer.eos_token, "").replace(self.processor.tokenizer.pad_token, "")
+            # seq = re.sub(r"<.*?>", "", seq, count=1).strip()  # remove first task start token
+            predictions.append(seq)
+
+        scores = []
+        for pred, answer in zip(predictions, answers):
+            pred = re.sub(r"(?:(?<=>) | (?=</s_))", "", pred)
+            answer = answer.replace(self.processor.tokenizer.eos_token, "") 
+
+            scores.append(edit_distance(pred, answer) / max(len(pred), len(answer)))
+
+            if self.config.get("verbose", False) and len(scores) == 1:
+                print(f"Prediction: {pred}")
+                print(f"    Answer: {answer}")
+                print(f" Normed ED: {scores[0]}")
+
+        self.log("val_edit_distance", np.mean(scores))
+        
+        return scores
+
+    def configure_optimizers(self):
+        # you could also add a learning rate scheduler if you want
+        optimizer = torch.optim.AdamW(self.parameters(), lr=self.config.get("lr"))
+    
+        return optimizer
+
+    def train_dataloader(self):
+        return DataLoader(train_dataset, collate_fn=train_collate_fn, batch_size=self.batch_size, shuffle=True, num_workers=4)
+
+    def val_dataloader(self):
+        return DataLoader(val_dataset, collate_fn=eval_collate_fn, batch_size=self.batch_size, shuffle=False, num_workers=4)
+
+config = {"max_epochs": 10,
+          # "val_check_interval": 0.2, # how many times we want to validate during an epoch
+          "check_val_every_n_epoch": 1,
+          "gradient_clip_val": 1.0,
+          "lr": 1e-4,
+          "batch_size": 4,
+          # "seed":2022,
+          "num_nodes": 1,
+          "warmup_steps": 50,
+          "result_path": "./result",
+          "verbose": True,
+}
+
+model_module = Idefics2ModelPLModule(config, processor, model)
+
+class PushToHubCallback(Callback):
+    def on_train_epoch_end(self, trainer, pl_module):
+        print(f"Pushing model to the hub, epoch {trainer.current_epoch}")
+        pl_module.model.push_to_hub("nielsr/idefics2-cord-demo",
+                                    commit_message=f"Training in progress, epoch {trainer.current_epoch}")
+
+    def on_train_end(self, trainer, pl_module):
+        print(f"Pushing model to the hub after training")
+        pl_module.processor.push_to_hub("nielsr/idefics2-cord-demo",
+                                    commit_message=f"Training done")
+        pl_module.model.push_to_hub("nielsr/idefics2-cord-demo",
+                                    commit_message=f"Training done")
+
+early_stop_callback = EarlyStopping(monitor="val_edit_distance", patience=3, verbose=False, mode="min")
+
+trainer = L.Trainer(
+        accelerator="gpu",
+        devices="1",
+        max_epochs=config.get("max_epochs"),
+        # val_check_interval=config.get("val_check_interval"),
+        check_val_every_n_epoch=config.get("check_val_every_n_epoch"),
+        gradient_clip_val=config.get("gradient_clip_val"),
+        precision="16-mixed", # we'll use mixed precision
+        num_sanity_val_steps=0,
+        logger=wandb_logger,
+        callbacks=[PushToHubCallback(), early_stop_callback],
+)
+
+trainer.fit(model_module)
