b8c9b2d63c35855815bdaaba572d48f08bee7e0d
Niels Rogge <niels.rogge1@gmail.com>
2025-09-26 21:24:38 +0200

    Add script

---
+++ src/transformers/models/eomt_dinov3/convert.sh
@@ -0,0 +1,6 @@
+python convert_eomt_dinov3_to_hf.py \
+    /Users/nielsrogge/Downloads/pytorch_model.bin \
+    . \
+    --backbone-repo-id facebook/dinov3-vits16-pretrain-lvd1689m \
+    --verify \
+    --original-repo-path /Users/nielsrogge/Documents/python_projects/eomt
