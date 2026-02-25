e1731478c779e60ab3559e613fcc7ebe598c1b6d
Niels <niels.rogge1@gmail.com>
2026-02-25 10:35:35 +0100

    Add id2label mappings

--- src/transformers/models/videomt/convert_videomt_to_hf.py
+++ src/transformers/models/videomt/convert_videomt_to_hf.py
@@ -50,14 +50,79 @@ MODEL_REPO_ID = "tue-mps/VidEoMT"
 
 # fmt: off
 CHECKPOINT_CONFIGS = {
-    "yt_2019_vit_small_52.8.pth":   {"image_size": 640,  "num_frames": 2, "hub_name": "videomt-dinov2-small-ytvis2019"},
-    "yt_2019_vit_base_58.2.pth":    {"image_size": 640,  "num_frames": 2, "hub_name": "videomt-dinov2-base-ytvis2019"},
-    "yt_2019_vit_large_68.6.pth":   {"image_size": 640,  "num_frames": 2, "hub_name": "videomt-dinov2-large-ytvis2019"},
-    "yt_2021_vit_large_63.1.pth":   {"image_size": 640,  "num_frames": 2, "hub_name": "videomt-dinov2-large-ytvis2021"},
-    "yt_2022_vit_large_42.6.pth":   {"image_size": 640,  "num_frames": 2, "hub_name": "videomt-dinov2-large-ytvis2022"},
-    "ovis_vit_large_52.5.pth":      {"image_size": 640,  "num_frames": 2, "hub_name": "videomt-dinov2-large-ovis"},
-    "vipseg_vit_large_55.2.pth":    {"image_size": 640,  "num_frames": 2, "hub_name": "videomt-dinov2-large-vipseg"},
-    "vspw_vit_large_95.0_64.9.pth": {"image_size": 1280, "num_frames": 2, "hub_name": "videomt-dinov2-large-vspw"},
+    "yt_2019_vit_small_52.8.pth":   {"image_size": 640,  "num_frames": 2, "hub_name": "videomt-dinov2-small-ytvis2019", "dataset": "ytvis_2019"},
+    "yt_2019_vit_base_58.2.pth":    {"image_size": 640,  "num_frames": 2, "hub_name": "videomt-dinov2-base-ytvis2019",  "dataset": "ytvis_2019"},
+    "yt_2019_vit_large_68.6.pth":   {"image_size": 640,  "num_frames": 2, "hub_name": "videomt-dinov2-large-ytvis2019", "dataset": "ytvis_2019"},
+    "yt_2021_vit_large_63.1.pth":   {"image_size": 640,  "num_frames": 2, "hub_name": "videomt-dinov2-large-ytvis2021", "dataset": "ytvis_2021"},
+    "yt_2022_vit_large_42.6.pth":   {"image_size": 640,  "num_frames": 2, "hub_name": "videomt-dinov2-large-ytvis2022", "dataset": "ytvis_2021"},
+    "ovis_vit_large_52.5.pth":      {"image_size": 640,  "num_frames": 2, "hub_name": "videomt-dinov2-large-ovis",      "dataset": "ovis"},
+    "vipseg_vit_large_55.2.pth":    {"image_size": 640,  "num_frames": 2, "hub_name": "videomt-dinov2-large-vipseg",    "dataset": "vipseg"},
+    "vspw_vit_large_95.0_64.9.pth": {"image_size": 1280, "num_frames": 2, "hub_name": "videomt-dinov2-large-vspw",      "dataset": "vipseg"},
+}
+
+YTVIS_2019_ID2LABEL = {
+    0: "person", 1: "giant_panda", 2: "lizard", 3: "parrot", 4: "skateboard",
+    5: "sedan", 6: "ape", 7: "dog", 8: "snake", 9: "monkey",
+    10: "hand", 11: "rabbit", 12: "duck", 13: "cat", 14: "cow",
+    15: "fish", 16: "train", 17: "horse", 18: "turtle", 19: "bear",
+    20: "motorbike", 21: "giraffe", 22: "leopard", 23: "fox", 24: "deer",
+    25: "owl", 26: "surfboard", 27: "airplane", 28: "truck", 29: "zebra",
+    30: "tiger", 31: "elephant", 32: "snowboard", 33: "boat", 34: "shark",
+    35: "mouse", 36: "frog", 37: "eagle", 38: "earless_seal", 39: "tennis_racket",
+}
+
+YTVIS_2021_ID2LABEL = {
+    0: "airplane", 1: "bear", 2: "bird", 3: "boat", 4: "car",
+    5: "cat", 6: "cow", 7: "deer", 8: "dog", 9: "duck",
+    10: "earless_seal", 11: "elephant", 12: "fish", 13: "flying_disc", 14: "fox",
+    15: "frog", 16: "giant_panda", 17: "giraffe", 18: "horse", 19: "leopard",
+    20: "lizard", 21: "monkey", 22: "motorbike", 23: "mouse", 24: "parrot",
+    25: "person", 26: "rabbit", 27: "shark", 28: "skateboard", 29: "snake",
+    30: "snowboard", 31: "squirrel", 32: "surfboard", 33: "tennis_racket", 34: "tiger",
+    35: "train", 36: "truck", 37: "turtle", 38: "whale", 39: "zebra",
+}
+
+OVIS_ID2LABEL = {
+    0: "Person", 1: "Bird", 2: "Cat", 3: "Dog", 4: "Horse",
+    5: "Sheep", 6: "Cow", 7: "Elephant", 8: "Bear", 9: "Zebra",
+    10: "Giraffe", 11: "Poultry", 12: "Giant_panda", 13: "Lizard", 14: "Parrot",
+    15: "Monkey", 16: "Rabbit", 17: "Tiger", 18: "Fish", 19: "Turtle",
+    20: "Bicycle", 21: "Motorcycle", 22: "Airplane", 23: "Boat", 24: "Vehical",
+}
+
+VIPSEG_ID2LABEL = {
+    0: "wall", 1: "ceiling", 2: "door", 3: "stair", 4: "ladder",
+    5: "escalator", 6: "Playground_slide", 7: "handrail_or_fence", 8: "window", 9: "rail",
+    10: "goal", 11: "pillar", 12: "pole", 13: "floor", 14: "ground",
+    15: "grass", 16: "sand", 17: "athletic_field", 18: "road", 19: "path",
+    20: "crosswalk", 21: "building", 22: "house", 23: "bridge", 24: "tower",
+    25: "windmill", 26: "well_or_well_lid", 27: "other_construction", 28: "sky", 29: "mountain",
+    30: "stone", 31: "wood", 32: "ice", 33: "snowfield", 34: "grandstand",
+    35: "sea", 36: "river", 37: "lake", 38: "waterfall", 39: "water",
+    40: "billboard_or_Bulletin_Board", 41: "sculpture", 42: "pipeline", 43: "flag", 44: "parasol_or_umbrella",
+    45: "cushion_or_carpet", 46: "tent", 47: "roadblock", 48: "car", 49: "bus",
+    50: "truck", 51: "bicycle", 52: "motorcycle", 53: "wheeled_machine", 54: "ship_or_boat",
+    55: "raft", 56: "airplane", 57: "tyre", 58: "traffic_light", 59: "lamp",
+    60: "person", 61: "cat", 62: "dog", 63: "horse", 64: "cattle",
+    65: "other_animal", 66: "tree", 67: "flower", 68: "other_plant", 69: "toy",
+    70: "ball_net", 71: "backboard", 72: "skateboard", 73: "bat", 74: "ball",
+    75: "cupboard_or_showcase_or_storage_rack", 76: "box", 77: "traveling_case_or_trolley_case", 78: "basket", 79: "bag_or_package",
+    80: "trash_can", 81: "cage", 82: "plate", 83: "tub_or_bowl_or_pot", 84: "bottle_or_cup",
+    85: "barrel", 86: "fishbowl", 87: "bed", 88: "pillow", 89: "table_or_desk",
+    90: "chair_or_seat", 91: "bench", 92: "sofa", 93: "shelf", 94: "bathtub",
+    95: "gun", 96: "commode", 97: "roaster", 98: "other_machine", 99: "refrigerator",
+    100: "washing_machine", 101: "Microwave_oven", 102: "fan", 103: "curtain", 104: "textiles",
+    105: "clothes", 106: "painting_or_poster", 107: "mirror", 108: "flower_pot_or_vase", 109: "clock",
+    110: "book", 111: "tool", 112: "blackboard", 113: "tissue", 114: "screen_or_television",
+    115: "computer", 116: "printer", 117: "Mobile_phone", 118: "keyboard", 119: "other_electronic_product",
+    120: "fruit", 121: "food", 122: "instrument", 123: "train",
+}
+
+DATASET_TO_ID2LABEL = {
+    "ytvis_2019": YTVIS_2019_ID2LABEL,
+    "ytvis_2021": YTVIS_2021_ID2LABEL,
+    "ovis": OVIS_ID2LABEL,
+    "vipseg": VIPSEG_ID2LABEL,
 }
 # fmt: on
 
@@ -318,6 +383,18 @@ def convert_checkpoint(
     config = infer_videomt_config(
         original_state_dict, checkpoint_filename, image_size=image_size, num_frames=num_frames
     )
+
+    ckpt_cfg = CHECKPOINT_CONFIGS.get(checkpoint_filename)
+    if ckpt_cfg is not None:
+        id2label = DATASET_TO_ID2LABEL[ckpt_cfg["dataset"]]
+        if len(id2label) != config.num_labels:
+            raise ValueError(
+                f"id2label length ({len(id2label)}) does not match num_labels ({config.num_labels}) "
+                f"for checkpoint '{checkpoint_filename}'."
+            )
+        config.id2label = id2label
+        config.label2id = {v: k for k, v in id2label.items()}
+
     model = VideomtForUniversalSegmentation(config)
     converted_state_dict, consumed_keys = convert_state_dict(original_state_dict)
 
